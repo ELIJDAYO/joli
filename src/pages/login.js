85776ead18fe881597019769a1c82d826aea3c87
ELIJDAYO <95513598+ELIJDAYO@users.noreply.github.com>
2023-03-15 06:12:59 +0800

    created login form

---
+++ src/pages/login.js
@@ -0,0 +1,74 @@
+import Layout from 'components/Layout';
+import Link from 'next/link';
+// Let's implement validation in the client site when we click on logging it, refresh the page.
+// But what we're going to do is to show error message to the user.
+// For this purpose, we are going to use React hook form.
+// It's a simple form validation with React Hook.
+import { useForm } from 'react-hook-form';
+
+export default function LoginScreen() {
+  const {
+    // check onSubmit which accepts params
+    handleSubmit,
+    register,
+    formState: { errors },
+  } = useForm();
+  const submitHandler = ({ email, password }) => {
+    console.log(email, password);
+  };
+  return (
+    <Layout title="Login">
+      <form
+        className="mx-auto max-w-screen-md"
+        onSubmit={handleSubmit(submitHandler)}
+      >
+        {/* xl -extra small */}
+        <h1 className="mb-4 text-xl">Login</h1>
+        <div className="mb-4">
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            // to register email and password in the input boxes.
+            // To do that, we use the register function from use form hook, go to the input box.
+            {...register('email', {
+              required: 'Please enter email',
+              pattern: {
+                value: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$/i,
+                message: 'Please enter valid email',
+              },
+            })}
+            className="w-full"
+            id="email"
+            autoFocus
+          ></input>
+          {errors.email && (
+            <div className="text-red-500">{errors.email.message}</div>
+          )}
+        </div>
+        <div className="mb-4">
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            {...register('password', {
+              required: 'Please enter password',
+              minLength: { value: 6, message: 'password is more than 5 chars' },
+            })}
+            className="w-full"
+            id="password"
+            autoFocus
+          ></input>
+          {errors.password && (
+            <div className="text-red-500 ">{errors.password.message}</div>
+          )}
+        </div>
+        <div className="mb-4 ">
+          <button className="primary-button">Login</button>
+        </div>
+        <div className="mb-4 ">
+          Don&apos;t have an account? &nbsp;
+          <Link href="register">Register</Link>
+        </div>
+      </form>
+    </Layout>
+  );
+}
