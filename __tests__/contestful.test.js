911d8ec9d68e22de57a5a7a698340d37da99faf6
DennisAlwyn <104767447+DennisAlwyn@users.noreply.github.com>
2023-03-22 14:23:03 +0800

    Added test for contentful.js

---
+++ __tests__/contestful.test.js
@@ -0,0 +1,15 @@
+import '@testing-library/jest-dom';
+import { Client } from '../api/contentful';
+
+describe('contentful', () => {
+  it('API that connects to the Contentful CMS', async () => {
+    const response = await Client.getEntries({
+        content_type: "category",
+        order: "fields.categoryName",
+        limit: 12,
+      });
+
+
+    await expect(response.items).toBeDefined();
+  })
+})
