0bfeab45b2ee9161681e3d298bae57b854873944
DennisAlwyn <104767447+DennisAlwyn@users.noreply.github.com>
2023-03-27 02:18:02 +0800

    Contentful Loader Page
    - Created page that loads products from Contentful into MongoDB. New products are missing images, prices, and description.

---
+++ src/pages/contentfulLoader.js
@@ -0,0 +1,61 @@
+import { Client } from "api/contentful";
+import Product from "models/Models";
+import { useEffect } from "react";
+import db from "utils/db";
+
+const contentfulLoader = () => {
+    useEffect(() => {
+        window.location.href = "/";
+    }, [])
+
+    return (
+        <div>Loading Data...</div>
+    );
+};
+
+export default contentfulLoader;
+
+export async function getServerSideProps () {
+    const response = await Client.getEntries({
+        content_type: "products",
+        limit: 1, //Remove this when testing is done
+      });
+    await db.connect();
+
+    const items = response.items
+
+    const products = [];
+
+    //Convert to format used in here
+    items.map((item) => {
+        let slugTemp = item.fields.name.replace(" ", "-").toLowerCase();
+
+        let tempProd = new Product({
+            name: item.fields.name,
+            slug: slugTemp,
+            category: item.fields.category[0].fields.categoryName,
+            image: "/images/default.jpg", //Default value
+            price: 0, //Default value
+            brand: item.fields.brand,
+            rating: 0, //Default value
+            numReviews: 0, //Default value
+            countInStock: 0, //Default value
+            description: "N/A", //Default value
+        });
+
+        products.push(tempProd);
+    });
+
+    console.log(products);
+
+    //Upsert to MongoDB
+    products.map(async (product) => {
+        await product.save();
+    });
+    
+    return {
+        props: {
+            ret: 0
+        },
+    };
+};
