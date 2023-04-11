b0d83a29eadb5309a36efae5cedaa8d886a8d01e
DennisAlwyn <104767447+DennisAlwyn@users.noreply.github.com>
2023-04-11 20:49:34 +0800

    Merging all tests into api and home test

--- __tests__/api.test.js
+++ __tests__/api.test.js
@@ -2,6 +2,7 @@ import '@testing-library/jest-dom';
 import Product from 'models/Models';
 import mongoose from 'mongoose';
 import { TextEncoder, TextDecoder } from 'util';
+import { Client } from '../api/contentful';
 Object.assign(global, { TextDecoder, TextEncoder });
 
 beforeAll(async () => {
@@ -15,3 +16,15 @@ describe('MongoDB', () => {
     await mongoose.connection.close();
   });
 });
+
+describe('contentful', () => {
+  it('Tests Contentful API connection', async () => {
+    const response = await Client.getEntries({
+        content_type: "category",
+        order: "fields.categoryName",
+        limit: 12,
+      });
+
+    await expect(response.items).toBeDefined();
+  })
+})
--- __tests__/ui/home.test.js
+++ __tests__/ui/home.test.js
@@ -1,15 +1,37 @@
 import '@testing-library/jest-dom';
 import { render } from '@testing-library/react';
 import CheckoutWizard from '../../components/CheckoutWizard';
+import AdminSidebar from "../../components/AdminSidebar"
 import { TextEncoder, TextDecoder } from 'util';
 Object.assign(global, { TextDecoder, TextEncoder });
 
 describe('CheckoutWizard', () => {
   it('Tests if component renders', () => {
-    //Might be a bit incomplete
     render(<CheckoutWizard activeStep={0} />);
     render(<CheckoutWizard activeStep={1} />);
     render(<CheckoutWizard activeStep={2} />);
     render(<CheckoutWizard activeStep={3} />);
   });
 });
+
+describe('AdminSidebar', () => {
+  it('Tests if component renders', async () => {
+      const test = render (<AdminSidebar CurrentPage="Dashboard"/>);
+      render (<AdminSidebar CurrentPage="Orders"/>);
+      render (<AdminSidebar CurrentPage="Products"/>);
+      render (<AdminSidebar CurrentPage="Users"/>);
+
+      await test.findAllByText("Dashboard").then((result) => {
+          expect(result.length).toBe(4);
+      });
+      await test.findAllByText("Orders").then((result) => {
+          expect(result.length).toBe(4);
+      });
+      await test.findAllByText("Products").then((result) => {
+          expect(result.length).toBe(4);
+      });
+      await test.findAllByText("Users").then((result) => {
+          expect(result.length).toBe(4);
+      });
+  })
+})
